5dafed0150bd794c4a3b3158bc1a122d60e3a300
Mohit Jain <mjainv5@gmail.com>
2025-10-17 12:17:15 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -110,7 +110,7 @@ export default function Home() {
           <Link
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             prefetch={false}
-            href="/custom-page"
+            href="https://www.makemytrip.com/hotels/budget-hotels-in-goa.html"
           >
             Visit Next
           </Link>
